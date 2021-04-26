0ac09608dd7928d43856bd7d3fbc666a8f40f368
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-27 01:10:08 +0530

    code added for painting with a particular color

--- draw.py
+++ draw.py
@@ -2,7 +2,10 @@ import cv2 as cv
 import numpy as np
 
 # creating a blank image (uint8 means an image)
-blank = np.zeros((500, 500), dtype="uint8")
-cv.imshow("Blank", blank)
+blank = np.zeros((500, 500, 3), dtype="uint8")
 
+# 1. Paint the image a certain colour
+blank[:] = 0, 255, 0  # painting the entire image green
+
+cv.imshow("Blank", blank)
 cv.waitKey(0)
