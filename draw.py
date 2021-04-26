3263e9e85f32a32dea19b411212c1507e000eb9e
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-27 01:16:19 +0530

    code added for drawing a rectangle

--- draw.py
+++ draw.py
@@ -5,7 +5,15 @@ import numpy as np
 blank = np.zeros((500, 500, 3), dtype="uint8")
 
 # 1. Paint the image a certain colour
-blank[:] = 0, 255, 0  # painting the entire image green
+# blank[:] = 0, 255, 0  # painting the entire image green
+# cv.imshow("Green", blank)
 
-cv.imshow("Blank", blank)
+# 2. Painting a certain portion of the image
+# blank[200:300, 300:400] = 0, 0, 255
+# cv.imshow('Multi', blank)
+
+# 3. Drawing a rectangle
+cv.rectangle(blank, (0, 0), (250, 250), (0, 255, 0), thickness=3)
+cv.imshow('Rectangle', blank)
+
 cv.waitKey(0)
