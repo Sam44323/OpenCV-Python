01ebc02a8719385ac8946ec04e4160a4033a5e76
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-27 01:58:05 +0530

    Did some refactoring

--- draw.py
+++ draw.py
@@ -5,30 +5,30 @@ import numpy as np
 blank = np.zeros((500, 500, 3), dtype="uint8")
 
 # 1. Paint the image a certain colour
-# blank[:] = 0, 255, 0  # painting the entire image green
-# cv.imshow("Green", blank)
+blank[:] = 0, 255, 0  # painting the entire image green
+cv.imshow("Green", blank)
 
 # 2. Painting a certain portion of the image
-# blank[200:300, 300:400] = 0, 0, 255
-# cv.imshow('Multi', blank)
+blank[200:300, 300:400] = 0, 0, 255
+cv.imshow('Multi', blank)
 
 # 3. Drawing a rectangle
-# cv.rectangle(blank, (0, 0), (250, 250), (0, 255, 0), thickness=3)
-# cv.imshow('Rectangle', blank)
+cv.rectangle(blank, (0, 0), (250, 250), (0, 255, 0), thickness=3)
+cv.imshow('Rectangle', blank)
 
 # 4. Filling the rectangle
-# cv.rectangle(blank, (0, 0), (250, 250), (0, 255, 0), thickness=cv.FILLED)
-# cv.imshow('Rectangle', blank)
+cv.rectangle(blank, (0, 0), (250, 250), (0, 255, 0), thickness=cv.FILLED)
+cv.imshow('Rectangle', blank)
 
 # 5. Drawing a circle
-# cv.circle(blank, (250, 250), 100, (0, 134, 145), thickness=-1)
-# # -1 also means FILLED
-# cv.imshow("Circle", blank)
+cv.circle(blank, (250, 250), 100, (0, 134, 145), thickness=-1)
+# -1 also means FILLED
+cv.imshow("Circle", blank)
 
 # 6. Drawing a line
-# cv.line(blank, (0, 0), (blank.shape[1]//2,
-#         blank.shape[0]//2), (0, 134, 145), thickness=3)
-# cv.imshow("Line", blank)
+cv.line(blank, (0, 0), (blank.shape[1]//2,
+        blank.shape[0]//2), (0, 134, 145), thickness=3)
+cv.imshow("Line", blank)
 
 # 7. Writing a text on an image
 cv.putText(blank, "Hello!", (225, 225),
