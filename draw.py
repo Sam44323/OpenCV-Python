82d23207176d3ea17d9844448c760c8f22c21a27
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-27 00:46:09 +0530

    blank image created for drawing

--- draw.py
+++ draw.py
@@ -1,7 +1,9 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('Photos/cat.jpeg')
-cv.imshow('Cat', img)
+# creating a blank image (uint8 means an image)
+blank = np.zeros((500, 500), dtype="uint8")
+cv.imshow("Blank", blank)
+
 
 cv.waitKey(0)
