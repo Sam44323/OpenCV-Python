28503a5ce2663eea0b9c480729d4e5d6147ba008
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-26 12:11:41 +0530

    File added for reading videos

--- readImage.py
+++ readImage.py
@@ -3,4 +3,5 @@ import cv2 as cv
 # reading an image and capturing as matrix of pixels it using opencv
 img = cv.imread('Photos/cat.jpeg')
 cv.imshow('Cat', img)
+
 cv.waitKey(0)
---
+++ readVideo.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+# reading videos in opencv
+# you will pass integer value to VideoCapture if you are using your webcam or camera
+
+capture = cv.VideoCapture('Videos/dog.mp4')
+
+while True:
+    isTure, frame = capture.read()  # getting the video frame by frame
+    cv.imshow('Video', frame)  # showing each frames
+
+    # this means if the letter d is pressed then stop displaying the video
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()  # release the capture device
+cv.destroyAllWindows()  # destoy the windows
