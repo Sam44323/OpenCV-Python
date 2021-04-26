d626cdf8176cc2df6ff5b3290925164a53ae429e
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-27 01:26:54 +0530

    logic added for filling shape with a color

--- draw.py
+++ draw.py
@@ -13,7 +13,11 @@ blank = np.zeros((500, 500, 3), dtype="uint8")
 # cv.imshow('Multi', blank)
 
 # 3. Drawing a rectangle
-cv.rectangle(blank, (0, 0), (250, 250), (0, 255, 0), thickness=3)
+# cv.rectangle(blank, (0, 0), (250, 250), (0, 255, 0), thickness=3)
+# cv.imshow('Rectangle', blank)
+
+# 4. Filling the rectangle
+cv.rectangle(blank, (0, 0), (250, 250), (0, 255, 0), thickness=cv.FILLED)
 cv.imshow('Rectangle', blank)
 
 cv.waitKey(0)
