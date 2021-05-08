80bfbf6a865ed1f1e44ab53bf017e028ca9d9a32
Suranjan <suranjanbiswas2002@gmail.com>
2021-05-08 15:14:42 +0530

    added logic for dilating an image

--- basicFunctions.py
+++ basicFunctions.py
@@ -15,8 +15,14 @@ blur = cv.GaussianBlur(img, (7, 7), cv.BORDER_DEFAULT)
 cv.imshow('Blurred', blur)
 
 # Edge cascade (finding the edges present in an image)
+# you can get rid of many edges by adding a lot of blur to the images
 
 canny = cv.Canny(blur, 125, 175)
 cv.imshow("Canny Edges Cascading", canny)
+
+# Dilating an image
+
+dilated = cv.dilate(canny, (7, 7), iterations=3)
+cv.imshow('Dilated ', dilated)
 
 cv.waitKey(0)
