74ecc60fc4bcc4c5f9ae1a28209681b3dde727a7
Suranjan <suranjanbiswas2002@gmail.com>
2021-05-08 14:22:42 +0530

    conversion from bgr to gray added

--- basicFunctions.py
+++ basicFunctions.py
@@ -1,1 +1,12 @@
 import cv2 as cv
+
+img = cv.imread('Photos/cat.jpeg')
+
+cv.imshow('Cats', img)
+
+# Converting an image(bgr) to grayscale
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Grayed Image', gray)
+
+cv.waitKey(0)
