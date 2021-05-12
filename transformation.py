e52d9141a9dbcedaf73531c2c40da2de18d43493
Suranjan <suranjanbiswas2002@gmail.com>
2021-05-12 14:16:09 +0530

    logic added for rotating

--- transformation.py
+++ transformation.py
@@ -22,5 +22,21 @@ def translate(img, x, y):
 translated = translate(img, 100, 100)
 cv.imshow('Translated', translated)
 
+# Rotation
+
+
+def rotate(img, angle, rotPoint=None):
+    (height, width) = img.shape[:2]
+
+    if rotPoint is None:
+        rotPoint = (width//2, height//2)
+
+    # creating a rotation matrix
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    return cv.warpAffine(img, rotMat, (img.shape[1], img.shape[0]))
+
+
+rotated = rotate(img, 45)
+cv.imshow('Rotated', rotated)
 
 cv.waitKey(0)
